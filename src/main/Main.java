7bb37e9597c8de6daadd27bf3f4c28c6a96b3aea
Rock <237129512@qq.com>
2023-06-18 08:58:53 +0800

    TopKSumTree

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,128 +1,146 @@
 package main;
-
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
+
+class TopKSumTree {
+
+    static class Node {
+        Node left;
+        Node right;
+        long sum;
+        long cnt;
+        int ls, rs;//debug用
+    }
+
+    int maxN;
+    Node root;
+
+    public TopKSumTree(int maxN) {
+        this.maxN = maxN;
+        this.root = new Node();
+        root.ls = 0;
+        root.rs = maxN;
+    }
+
+    long trim(long v) {
+        //可以加上取余
+        return v;
+    }
+
+    public void add(int i, long val, long cnt) {
+        add(root, i, val, cnt, 0, maxN);
+    }
+
+    /**
+     * 当前Node的范围: [ls,rs]
+     */
+    private void add(Node node, int i, long val, long cnt, int ls, int rs) {
+        if (i<0 || i > maxN){
+            throw new IllegalArgumentException("index:" + i);
+        }
+        if (rs == ls && ls == i) {
+            node.sum = trim(node.sum + val);
+            node.cnt = trim(node.cnt + cnt);
+            return;
+        }
+
+        pushDown(node, ls, rs);
+        int mid = ls + rs >> 1;
+        //左子树[ls,mid]
+        //右子树[mid+1,rs]
+        if (i <= mid) {
+            add(node.left, i, val, cnt, ls, mid);
+        }
+        if (i >= mid + 1) {
+            add(node.right, i, val, cnt,mid + 1, rs);
+        }
+        node.sum = trim(node.sum + val);
+        node.cnt = trim(node.cnt + cnt);
+    }
+
+    void pushDown(Node node, int ls, int rs) {
+        int mid = ls + rs >> 1;
+        if (node.left == null) {
+            node.left = new Node();
+            node.left.ls = ls;
+            node.left.rs = mid;
+        }
+        if (node.right == null) {
+            node.right = new Node();
+            node.right.ls = mid + 1;
+            node.right.rs = rs;
+        }
+    }
+
+    public long sum(int l, int r) {
+        return sum(root, l, r, 0, maxN);
+    }
+
+    private long sum(Node node, int l, int r, int ls, int rs) {
+        if (r<l) {
+            return 0;
+        }
+        if (r<0){
+            throw new IllegalArgumentException("index:"+r);
+        }
+        if (l <= ls && rs <= r) {
+            return node.sum;
+        }
+        pushDown(node, ls, rs);
+        int mid = ls + rs >> 1;
+        long res = 0;
+        if (l <= mid) {
+            res += sum(node.left, l, r, ls, mid);
+        }
+        if (r >= mid + 1) {
+            res += sum(node.right, l, r, mid + 1, rs);
+        }
+        return trim(res);
+    }
+
+    //从最大下标开始向左找到cnt个值并返回sum
+    public long sumLeftwardWithCnt(Node node, long cnt) {
+        if (cnt==0) return 0;
+        int ls=node.ls, rs=node.rs;
+        if (ls==rs && node.cnt>cnt) {
+            return node.sum / node.cnt * cnt;
+        }
+        if (node.cnt<=cnt) {
+            return node.sum;
+        }
+        pushDown(node, ls, rs);
+        long res=0;
+        if (node.right.cnt < cnt) {
+            res = trim(res + node.right.sum + sumLeftwardWithCnt(node.left, cnt - node.right.cnt));
+        } else {
+            res = sumLeftwardWithCnt(node.right, cnt);
+        }
+        return res;
+    }
+}
+
 
 public class Main {
-    int mod = 998244353;
-    static long ksm(long a, long pow, long mod) {
-        long skt = 1;
-        while (pow > 0) {
-            if (pow % 2 != 0) {
-                skt = skt * a % mod;
-            }
-            a = a * a % mod;
-            pow = pow >> 1;
-        }
-        return skt % mod;
-    }
-
-    String s;
-    int n,b,p,k;
     public void solve() throws Exception {
-        n=nextInt();
-        b=nextInt();
-        p=nextInt();
-        k=nextInt();
-        s=next();
-        int sh=0;
-        for (int i = 0; i < n; i++) {
-            int c = s.charAt(i) - 'a' + 1;
-            int ksm = (int) ksm(b, n - i - 1, p);
-            sh = (sh + c*ksm) % p;
-        }
-        Node node = dfs(n - 1, k, sh);
-        if (node==null) {
-            System.out.println(-1);
-        } else {
-            List<Character> res=new ArrayList<>();
-            while (node!=null) {
-                res.add(node.c);
-                node=node.node;
+        int n = nextInt(),k=nextInt(),q=nextInt();
+        int MX = (int) 1e9;
+        TopKSumTree tree = new TopKSumTree((int) 1e9);
+        int[] ar=new int[n];
+        Arrays.fill(ar,-1);
+        for (int i = 0; i < q; i++) {
+            int j=nextInt()-1,y=nextInt();
+            int old = ar[j];
+            ar[j] = y;
+            if (old>-1) {
+                tree.add(old,-old,-1);
             }
-            res.remove(n);
-            Collections.reverse(res);
-            System.out.println(res.stream().map(x->x.toString()).collect(Collectors.joining()));
+            tree.add(y,y,1);
+            out.println(tree.sumLeftwardWithCnt(tree.root, k));
         }
-    }
-
-    static class Key {
-        int i,k,hash;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Key key = (Key) o;
-            return i == key.i && k == key.k && hash == key.hash;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(i, k, hash);
-        }
-
-        public Key(int i, int k, int hash) {
-            this.i = i;
-            this.k = k;
-            this.hash = hash;
-        }
-    }
-    static class Node {
-        Node node;
-        char c;
-
-        public Node(Node node, char c) {
-            this.node = node;
-            this.c = c;
-        }
-    }
-    static Node NULL = new Node(null,'a');
-    Map<Key,Node> map = new HashMap<>();
-    Node dfs(int i, int k, int hash) {
-        if (i+1<k) {
-            return null;
-        }
-        Key key=new Key(i,k,hash);
-        if (i==-1) {
-            if (k==0&&hash==0) {
-                return new Node(null,'0');
-            }
-            return null;
-        }
-        Node node = map.get(key);
-        if (node==NULL) return null;
-        if (node!=null){
-            return node;
-        }
-        for (int j = 1; j <= 26; j++) {
-            int nhash = (int) ((hash - j * ksm(b,n-i-1,p) + 30 * p) % p);
-            if (j==s.charAt(i)-'a'+1) {
-                Node r = dfs(i - 1, k - 1, nhash);
-                if (r != null) {
-                    Node node1 = new Node(r, (char) (j + 'a' - 1));
-                    map.put(key, node1);
-                    return node1;
-                }
-            } else {
-                Node r = dfs(i - 1, k, nhash);
-                if (r != null) {
-                    Node node1 = new Node(r, (char) (j + 'a' - 1));
-                    map.put(key, node1);
-                    return node1;
-                }
-            }
-        }
-        map.put(key, NULL);
-        return null;
     }
 
     public static void main(String[] args) throws Exception {
-        int t = nextInt();
-        for (int i = 0; i < t; i++) {
-            new Main().solve();
-        }
+        new Main().solve();
     }
 
     static PrintWriter out = new PrintWriter(System.out, true);
--- src/main/lc/LC.java
+++ src/main/lc/LC.java
@@ -9,29 +9,56 @@ import java.util.*;
  * @Date 2023/4/22
  */
 public class LC {
-class SumSegTree {
+
+    public class MinValSegTree {
 
     class Node {
         Node left;
         Node right;
+        long lazyAdd;
         long sum;
-        long lazy;
         int ls, rs;//debug用
+        int minId;//值最小且index最小的index
+        long minVal;
     }
 
     int maxN;
     Node root;
 
-    public SumSegTree(int maxN) {
+    public MinValSegTree(int maxN) {
         this.maxN = maxN;
-        this.root = new Node();
+        this.root = createNode(0, maxN);
         root.ls = 0;
         root.rs = maxN;
     }
 
-    long trim(long v) {
-        //可以加上取余
-        return v;
+    Node createNode(int ls, int rs) {
+        Node r = new Node();
+        r.minId = ls;
+        return r;
+    }
+
+    /**
+     * 添加val到当前node的整个区间，不用下发
+     */
+    void addLazy(Node node, int ls, int rs, long val) {
+        node.lazyAdd += val;
+        node.sum += (rs - ls + 1) * val;
+        node.minVal += val;
+    }
+
+    /**
+     * child修改后，更新统计到当前node，node中可以有多个需要reduce的字段. 此时Node的lazy值已下发。
+     */
+    void reduce(Node node, int ls, int rs) {
+        node.sum = node.left.sum + node.right.sum;
+        if (node.right.minVal < node.left.minVal) {
+            node.minId = node.right.minId;
+            node.minVal = node.right.minVal;
+        } else {
+            node.minId = node.left.minId;
+            node.minVal = node.left.minVal;
+        }
     }
 
     public void add(int l, int r, long val) {
@@ -42,16 +69,12 @@ class SumSegTree {
      * 当前Node的范围: [ls,rs]
      */
     private void add(Node node, int l, int r, long val, int ls, int rs) {
-        if (r<l){
-            return;
-        }
-        if (r<0){
-            throw new IllegalArgumentException("index:"+r);
+        if (l < 0 || r > maxN) {
+            throw new IllegalArgumentException();
         }
         if (l <= ls && rs <= r) {
             //[l,r]覆盖了当前子树
-            node.sum = trim(node.sum+(rs - ls + 1) * val);
-            node.lazy = trim(node.lazy + val);
+            addLazy(node, ls, rs, val);
             return;
         }
 
@@ -65,23 +88,28 @@ class SumSegTree {
         if (r >= mid + 1) {
             add(node.right, l, r, val, mid + 1, rs);
         }
-        int cover=Math.min(r,rs)-Math.max(l,ls)+1;
-        node.sum = trim(node.sum + val * cover);
+        reduce(node, ls, rs);
     }
 
+    //下发lazy值
     void pushDown(Node node, int ls, int rs) {
         int mid = ls + rs >> 1;
         if (node.left == null) {
-            node.left = new Node();
+            node.left = createNode(ls, mid);
             node.left.ls = ls;
             node.left.rs = mid;
         }
         if (node.right == null) {
-            node.right = new Node();
+            node.right = createNode(mid + 1, rs);
             node.right.ls = mid + 1;
             node.right.rs = rs;
         }
-        //pushDown的时候并没有去下发lazy值,因为对于求sum来说,可以在query的时候把整条路径上的lazy都算上
+        if (node.lazyAdd != 0) {
+            //lazy是当前节点已经算了，但是还没下发child的
+            addLazy(node.left, ls, mid, node.lazyAdd);
+            addLazy(node.right, mid + 1, rs, node.lazyAdd);
+            node.lazyAdd = 0;
+        }
     }
 
     public long sum(int l, int r) {
@@ -89,60 +117,93 @@ class SumSegTree {
     }
 
     private long sum(Node node, int l, int r, int ls, int rs) {
-        if (r<l) {
-            return 0;
-        }
-        if (r<0){
-            throw new IllegalArgumentException("index:"+r);
+        if (l < 0 || r > maxN) {
+            throw new IllegalArgumentException();
         }
         if (l <= ls && rs <= r) {
             return node.sum;
         }
         pushDown(node, ls, rs);
         int mid = ls + rs >> 1;
-        int cover=Math.min(r,rs)-Math.max(l,ls)+1;
-        long res = trim(cover*node.lazy);
+        long res = 0;
         if (l <= mid) {
             res += sum(node.left, l, r, ls, mid);
         }
         if (r >= mid + 1) {
             res += sum(node.right, l, r, mid + 1, rs);
         }
-        return trim(res);
+        return res;
     }
-}
 
-class Solution {
-    public long countOperationsToEmptyArray(int[] nums) {
-        int n = nums.length;
-        SumSegTree tree=new SumSegTree(n-1);
-        List<int[]> list=new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            tree.add(i,i,1);
-            list.add(new int[]{nums[i],i});
-        }
-        Collections.sort(list, Comparator.comparingInt(x->x[0]));
-        int last=0;
-        int res=0;
-        for (int[] ints : list) {
-            int e=ints[0],i=ints[1];
-            tree.add(i,i,-1);
-            if (i>=last) {
-                res+=tree.sum(last,i);
-            } else {
-                res+=tree.sum(0,n-1)-tree.sum(i,last);
+    //查询最小值
+    public long queryMinVal(int l, int r) {
+        tmpMinId = -1;
+        tmpMinVal = Long.MAX_VALUE;
+        queryMinId(root, l, r, 0, maxN);
+        return tmpMinVal;
+    }
+
+    //查询最小值的下标，如果有多个则取下标最小的
+    public int queryMinId(int l, int r) {
+        tmpMinId = -1;
+        tmpMinVal = Long.MAX_VALUE;
+        queryMinId(root, l, r, 0, maxN);
+        return tmpMinId;
+    }
+
+    int tmpMinId;
+    long tmpMinVal;
+
+    private void queryMinId(Node node, int l, int r, int ls, int rs) {
+        if (l <= ls && rs <= r) {
+            if (tmpMinId == -1 || node.minVal < tmpMinVal || node.minVal == tmpMinVal && node.minId < tmpMinId) {
+                tmpMinId = node.minId;
+                tmpMinVal = node.minVal;
             }
-            last=i;
+            return;
         }
-        return res+n;
+        pushDown(node, ls, rs);
+        int mid = ls + rs >> 1;
+        if (l <= mid) {
+            queryMinId(node.left, l, r, ls, mid);
+        }
+        if (r >= mid + 1) {
+            queryMinId(node.right, l, r, mid + 1, rs);
+        }
     }
 }
 
+    class Solution {
+        public long minCost(int[] nums, int x) {
+            int n= nums.length;;
+            MinValSegTree tree = new MinValSegTree(n);
+            for (int i = 0; i < n; i++) {
+                tree.add(i,i,nums[i]);
+            }
+            long res= (long) 1e18;
+            for (int d = 0; d < n; d++) {
+                long s=0;
+                for (int i = 0; i < n; i++) {
+                    if (i+d>=n){
+                        s+=Math.min(tree.queryMinVal(i,n-1),tree.queryMinVal(0,i+d-n));
+                    } else {
+                        s+=tree.queryMinVal(i,i+d);
+                    }
+                }
+                res=Math.min(res,s+ (long) d * (long)x);
+            }
+            return res;
+        }
+    }
 
     public static void main(String[] args) {
 //        long l = new LC().new Solution().countOperationsToEmptyArray(LCUtils.parseIntAr("[4,3,2,1]"));
-        long l = new LC().new Solution().countOperationsToEmptyArray(LCUtils.parseIntAr("[1,4,3,2]"));
-        System.out.println(l);
+//        int[] ints = new LC().new Solution().maximumSumQueries(
+//                LCUtils.parseIntAr("[4,3,1,2]"),
+//                LCUtils.parseIntAr("[2,4,9,5]"),
+//                LCUtils.parseIntIntAr("[[4,1],[1,3],[2,5]]")
+//        );
+//        System.out.println(Arrays.toString(ints));
     }
 
 }
