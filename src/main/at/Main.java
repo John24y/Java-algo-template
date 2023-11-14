67fc04d50a7f423046475b4fb95ea721f60bda40
crow <g1295912@gmail.com>
2023-11-14 16:10:56 +0800

    并查集增强

--- src/main/at/Main.java
+++ src/main/at/Main.java
@@ -2,189 +2,70 @@ package main.at;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
-class MaxValSegTree {
-    //对查询结果相加需要注意溢出
-    static final long INIT = 0;
-    static final byte OP_ADD = 1;
-    static final byte OP_SET = 2;
+class HeuUnionFind {
+    int[] fa;
+    int[] sz;//包含节点数量
+    long[] offset; // 每个节点相比于当前根节点的增量, offset[i]=val[i]-val[root[i]]
+    List<List<Integer>> vertex = new ArrayList<>();
 
-    long[] maxVal;
-    int[] maxId;
-    byte[] lazyType;
-    long[] lazyVal;
-    long[] sum;
-
-    int maxN, varStart, varIdx;
-
-    public MaxValSegTree(int maxN, long[] initVals) {
-        this.maxN = maxN;
-        varIdx = varStart = Integer.highestOneBit(maxN) << 2;
-        int len = varStart + 36;
-        maxVal = new long[len];
-        maxId = new int[len];
-        lazyType = new byte[len];
-        lazyVal = new long[len];
-        sum = new long[len];
-        //build(1, initVals, 0, maxN);
-        for (int i = 0; i < maxId.length; i++) {
-            maxId[i]=i;
+    public HeuUnionFind(int n) {
+        fa=new int[n+1];
+        sz=new int[n+1];
+        offset=new long[n+1];
+        for (int i = 0; i <= n; i++) {
+            fa[i]=i;
+            sz[i]=1;
+            vertex.add(new ArrayList<>());
+            vertex.get(i).add(i);
         }
     }
 
-    private void build(int i, long[] vals, int ls, int rs) {
-        if (ls == rs) {
-            maxId[i] = ls;
-            maxVal[i] = vals == null ? INIT : vals[ls];
-            return;
+    int find(int x) {
+        if (fa[x]!=x){
+            int r = find(fa[x]);
+            fa[x]=r;
         }
-        int mid = ls + rs >> 1;
-        build(i * 2, vals, ls, mid);
-        build(i * 2 + 1, vals, mid + 1, rs);
-        reduce(i, i * 2, i * 2 + 1, ls, rs);
+        return fa[x];
     }
 
-    void apply(int i, int ls, int rs, byte type, long val) {
-        lazyType[i] = type;
-        if (type == OP_ADD) {
-            lazyVal[i] += val;
-            sum[i] += (rs - ls + 1) * val;
-            maxVal[i] += val;
-        } else if (type == OP_SET) {
-            lazyVal[i] = val;
-            sum[i] = (rs - ls + 1) * val;
-            maxVal[i] = val;
+    //val[j]-val[i]=d
+    boolean union(int i, int j, long d) {
+        int f1=find(i);
+        int f2=find(j);
+        if (f1==f2) {
+            return offset[j]-offset[i]==d;
         }
-    }
-
-    void reduce(int i, int left, int right, int ls, int rs) {
-        sum[i] = sum[left] + sum[right];
-        if (maxVal[right] > maxVal[left]) {
-            maxId[i] = maxId[right];
-            maxVal[i] = maxVal[right];
-        } else {
-            maxId[i] = maxId[left];
-            maxVal[i] = maxVal[left];
+        if (sz[f1]<sz[f2]) {
+            return union(j,i,-d);
         }
-    }
-
-    public void add(int l, int r, long val) {
-        update(1, l, r, 0, maxN, OP_ADD, val);
-    }
-
-    public void set(int l, int r, long val) {
-        update(1, l, r, 0, maxN, OP_SET, val);
-    }
-
-    /**
-     * 当前Node的范围: [ls,rs]
-     */
-    private void update(int i, int l, int r, int ls, int rs, byte type, long val) {
-        if (l < 0 || r > maxN) {
-            throw new IllegalArgumentException();
+        long nd = d-offset[j]+offset[i];
+        fa[f2]=f1;
+        sz[f1]+=sz[f2];
+        for (Integer v : vertex.get(f2)) {
+            offset[v]+=nd;
+            vertex.get(f1).add(v);
         }
-        if (l <= ls && rs <= r) {
-            apply(i, ls, rs, type, val);
-            return;
-        }
-
-        pushDown(i, ls, rs);
-        int mid = ls + rs >> 1;
-        //左子树[ls,mid]
-        //右子树[mid+1,rs]
-        if (l <= mid) {
-            update(i * 2, l, r, ls, mid, type, val);
-        }
-        if (r >= mid + 1) {
-            update(i * 2 + 1, l, r, mid + 1, rs, type, val);
-        }
-        reduce(i, i * 2, i * 2 + 1, ls, rs);
-    }
-
-    //下发lazy值
-    void pushDown(int i, int ls, int rs) {
-        int mid = ls + rs >> 1;
-        if (lazyType[i] != 0) {
-            apply(i * 2, ls, mid, lazyType[i], lazyVal[i]);
-            apply(i * 2 + 1, mid + 1, rs, lazyType[i], lazyVal[i]);
-            lazyType[i] = 0;
-            lazyVal[i] = 0;
-        }
-    }
-
-    public long sum(int l, int r) {
-        varIdx = varStart;
-        return sum[query(1, l, r, 0, maxN)];
-    }
-
-    //查询最大值
-    public long queryMaxVal(int l, int r) {
-        varIdx = varStart;
-        return maxVal[query(1, l, r, 0, maxN)];
-    }
-
-    //查询最大值的下标，如果有多个则取下标最小的
-    public int queryMaxId(int l, int r) {
-        varIdx = varStart;
-        return maxId[query(1, l, r, 0, maxN)];
-    }
-
-    private void cleanNode(int i) {
-        maxVal[i]=INIT;
-        maxId[i]=-1;
-    }
-
-    private int query(int i, int l, int r, int ls, int rs) {
-        if (l < 0 || r > maxN) {
-            throw new IllegalArgumentException();
-        }
-        if (l <= ls && rs <= r) {
-            return i;
-        }
-        pushDown(i, ls, rs);
-        int res = ++varIdx;
-        cleanNode(res);
-        int leftRes=varStart, rightRes=varStart;
-        int mid = ls + rs >> 1;
-        if (l <= mid) {
-            leftRes=query(i*2, l, r, ls, mid);
-        }
-        if (r >= mid + 1) {
-            rightRes=query(i*2+1, l, r, mid + 1, rs);
-        }
-        reduce(res, leftRes, rightRes, ls, rs);
-        return res;
+        vertex.get(f2).clear();
+        return true;
     }
 
 }
+
 public class Main {
 
     public void solve() throws Exception {
-        MaxValSegTree tree = new MaxValSegTree((int) 2e5+10, null);
-        int n=nextInt(),d=nextInt(),w=nextInt();
-        List<List<Integer>> xs = new ArrayList<>();
-        for (int i = 0; i < 2e5 + 10; i++) {
-            xs.add(new ArrayList<>());
-        }
-        for (int i = 0; i < n; i++) {
-            int t=nextInt(),x=nextInt();
-            xs.get(x).add(t);
-        }
-        int r=0;
-        long ans=0;
-        for (int l = 0; l < xs.size(); l++) {
-            while (r<xs.size()&&r<l+w){
-                for (Integer t : xs.get(r)) {
-                    tree.add(Math.max(0,t-d+1),t,1);
-                }
-                r++;
-            }
-            ans=Math.max(ans, tree.maxVal[1]);
-            for (Integer t : xs.get(l)) {
-                tree.add(Math.max(0,t-d+1),t,-1);
+        int n=nextInt(),q=nextInt();
+        HeuUnionFind find = new HeuUnionFind(n+1);
+        for (int i = 1; i <= q; i++) {
+            int a=nextInt(),b=nextInt();
+            long d=nextLong();
+            if (find.union(a,b,-d)){
+                out.print((i)+" ");
             }
         }
-        out.println(ans);
+        out.println();
     }
 
     public static void main(String[] args) throws Exception {
--- src/template/graph/UnionFind.java
+++ src/template/graph/UnionFind.java
@@ -1,5 +1,8 @@
 package template.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class UnionFind {
     int[] fa;
     int[] sz;//包含节点数量
@@ -23,13 +26,8 @@ class UnionFind {
     }
 
     void union(int i, int j) {
-        int f1=find(fa[i]);
-        int f2=find(fa[j]);
-        if (f1>f2){
-            int t=f2;
-            f2=f1;
-            f1=t;
-        }
+        int f1=find(i);
+        int f2=find(j);
         edgeCnt[f1]++;
         if (f1==f2)return;
         fa[f2]=f1;
@@ -38,3 +36,100 @@ class UnionFind {
     }
 
 }
+
+
+/**
+ * 可以对联通子集的根节点做区间修改 O(1)，修改的值在路径压缩时懒传递到子节点。
+ * 但修改时对整个联通子集的，对联通集的子树做修改是无法 O(1) 的，因为联通集内部的结构不稳定。
+ */
+class LazyUnionFind {
+    int[] fa;
+    int[] sz;//包含节点数量
+    long[] offset;// 每个节点相比于当前根节点的增量，offset[i]=val[i]-val[root[i]]
+
+    public LazyUnionFind(int n) {
+        fa=new int[n+1];
+        sz=new int[n+1];
+        offset=new long[n+1];
+        for (int i = 0; i <= n; i++) {
+            fa[i]=i;
+            sz[i]=1;
+        }
+    }
+
+    int find(int x) {
+        if (fa[x]!=x){
+            int r = find(fa[x]);
+            offset[x]=offset[fa[x]]+offset[x];
+            fa[x]=r;
+        }
+        return fa[x];
+    }
+
+    //val[j]-val[i]=d
+    boolean union(int i, int j, long d) {
+        int f1=find(i);
+        int f2=find(j);
+        if (f1==f2) {
+            return offset[j]-offset[i]==d;
+        }
+        fa[f2]=f1;
+        sz[f1]+=sz[f2];
+        offset[f2]=d-offset[j]+offset[i];
+        return true;
+    }
+}
+
+/**
+ * 启发式合并的并查集，每次小的联通集合并到大的，合并时可以遍历小联通集的每个节点
+ */
+class HeuUnionFind {
+    int[] fa;
+    int[] sz;//包含节点数量
+    long[] offset; // 每个节点相比于当前根节点的增量, offset[i]=val[i]-val[root[i]]
+    List<List<Integer>> vertex = new ArrayList<>();
+
+    public HeuUnionFind(int n) {
+        fa=new int[n+1];
+        sz=new int[n+1];
+        offset=new long[n+1];
+        for (int i = 0; i <= n; i++) {
+            fa[i]=i;
+            sz[i]=1;
+            vertex.add(new ArrayList<>());
+            vertex.get(i).add(i);
+        }
+    }
+
+    int find(int x) {
+        if (fa[x]!=x){
+            int r = find(fa[x]);
+            fa[x]=r;
+        }
+        return fa[x];
+    }
+
+    //val[j]-val[i]=d
+    boolean union(int i, int j, long d) {
+        int f1=find(i);
+        int f2=find(j);
+        if (f1==f2) {
+            return offset[j]-offset[i]==d;
+        }
+        if (sz[f1]<sz[f2]) {
+            return union(j,i,-d);
+        }
+        long nd = d-offset[j]+offset[i];
+        fa[f2]=f1;
+        sz[f1]+=sz[f2];
+        for (Integer v : vertex.get(f2)) {
+            offset[v]+=nd;
+            vertex.get(f1).add(v);
+        }
+        vertex.get(f2).clear();
+        return true;
+    }
+
+}
+
+
